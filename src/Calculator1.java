6aeb2d739cd6a762d115adb912ce309d724a7529
jeonjunbum <sky195153@navr.com>
2024-10-21 22:36:52 +0900

    디스플레이 필드 추가

--- src/Calculator1.java
+++ src/Calculator1.java
@@ -2,16 +2,31 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Calculator1 extends JFrame {
+    private JTextField displayField;
+
     Calculator1() {
-        setTitle("Calculator");
+        setTitle("calculator");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(500, 700);
         setLayout(new BorderLayout());
+        showNorth();
         setVisible(true);
+    }
+
+    void showNorth() {
+        JPanel p1 = new JPanel();
+        p1.setLayout(new BoxLayout(p1, BoxLayout.Y_AXIS));
+
+        displayField = new JTextField("0");
+        displayField.setHorizontalAlignment(JTextField.RIGHT);
+        displayField.setEditable(false);
+        displayField.setFont(new Font("SansSerif", Font.PLAIN, 20));
+        displayField.setPreferredSize(new Dimension(500, 100));
+        p1.add(displayField);
+        add(p1, BorderLayout.NORTH);
     }
 
     public static void main(String[] args) {
         new Calculator1();
     }
 }
-
